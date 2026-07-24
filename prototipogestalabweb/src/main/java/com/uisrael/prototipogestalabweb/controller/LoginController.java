852dcc868c23d0df9acd5a9c316c3de516fd4c13
FJFC003 <e1729156727@uisrael.edu.ec>
2026-07-23 22:04:41 -0500

    correcion

--- prototipogestalabweb/src/main/java/com/uisrael/prototipogestalabweb/controller/LoginController.java
+++ prototipogestalabweb/src/main/java/com/uisrael/prototipogestalabweb/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.uisrael.prototipogestalabweb.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +13,7 @@ import com.uisrael.prototipogestalabweb.services.ILoginService;
 
 import jakarta.servlet.http.HttpSession;
 
+@Controller
 public class LoginController {
 	
 	private final ILoginService loginService;
