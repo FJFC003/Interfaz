4d886e9949a420fa1095c1ab66cda4263f949134
FJFC003 <e1729156727@uisrael.edu.ec>
2026-07-27 00:17:29 -0500

    c

--- prototipogestalabweb/src/main/java/com/uisrael/prototipogestalabweb/configuration/WebClientConfig.java
+++ prototipogestalabweb/src/main/java/com/uisrael/prototipogestalabweb/configuration/WebClientConfig.java
@@ -1,11 +1,15 @@
 package com.uisrael.prototipogestalabweb.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+	
+	@Value("${gestalab.api.base-url}")
+    private String baseUrl;
 	
 	@Bean
 	WebClient webClient(WebClient.Builder builder) {
